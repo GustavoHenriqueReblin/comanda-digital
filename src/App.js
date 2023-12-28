f8adb64f03b06e7d7bdba694c1de753e5561ddb3
Gustavo <gustavohique12@gmail.com>
2023-12-28 11:24:35 -0300

    feat: Integrando ao graphql para teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GetUsers = gql`
+  query {
+    users {
+      id
+      username
+      password
+      token
+    }
+  }
+`;
+
 function App() {
+  function DisplayUsers() {
+    const { loading, error, data } = useQuery(GetUsers);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+  
+    return data.users.map(({ id, username, password, token }) => (
+      <div key={id}>
+        <h3>{username}</h3>
+        <br />
+        <p>{password}</p>
+        <br />
+      </div>
+    ));
+  }
 
   return (
-    <div></div>
+    <div>
+      <DisplayUsers />
+    </div>
   );
 }
 
