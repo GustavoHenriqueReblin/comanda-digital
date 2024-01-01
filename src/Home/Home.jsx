fa8eb12d7364a4cd6b91f04b252e5084f2c020c8
Gustavo <gustavohique12@gmail.com>
2024-01-01 13:27:16 -0300

    fix: Removido useEffect da home pois como é uma PrivateRoute já possui validação de login

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,27 +1,10 @@
-import Loading from '../components/loading';
-
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
-
 function Home() {
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!loading && !Cookies.get(process.env.REACT_APP_COOKIE_NAME_USER_TOKEN)) {
-      navigate('/login'); 
-    }
-    setLoading(false);
-  }, [loading, navigate]);
 
   return (
     <>
-      { loading ? <Loading /> : 
-        <h1>
+      <h1>
           Home
         </h1>
-      }
     </>
   );
 }
