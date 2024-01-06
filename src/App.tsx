96c396755410c524b14400c85690628f4c2e04cf
Gustavo <gustavohique12@gmail.com>
2024-01-05 21:03:34 -0300

    fix: Faltou comentar algumas partes referentes a inativação do login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import Cookies from 'js-cookie';
-import Login from './pages/Login/Login';
+// import Cookies from 'js-cookie';
+// import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
-import Admin from './pages/Admin/Admin';
+// import Admin from './pages/Admin/Admin';
 import Header from "./components/Header/Header";
 import ResponsiveProvider from "./components/ResponsiveProvider";
 
-interface PrivateRouteProps {
-  children: React.ReactNode;
-  redirectTo: string;
-}
+// interface PrivateRouteProps {
+//   children: React.ReactNode;
+//   redirectTo: string;
+// }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, redirectTo }) => {
-  const cookieName = process.env.REACT_APP_COOKIE_NAME_USER_TOKEN;
-  const isAuthenticated = !!(Cookies.get(cookieName ? cookieName : ''));
-  return isAuthenticated ? <>{children}</> : <Navigate to={redirectTo} />;
-}
+// const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, redirectTo }) => {
+//   const cookieName = process.env.REACT_APP_COOKIE_NAME_USER_TOKEN;
+//   const isAuthenticated = !!(Cookies.get(cookieName ? cookieName : ''));
+//   return isAuthenticated ? <>{children}</> : <Navigate to={redirectTo} />;
+// }
  
 function App() {
   return (
