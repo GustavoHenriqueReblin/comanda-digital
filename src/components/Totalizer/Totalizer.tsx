bc563d633f2762ca801405b383f5a62706a648c0
Gustavo <gustavohique12@gmail.com>
2024-01-14 00:02:40 -0300

    feat: refatorado layout do totalizador e adicionado botão para confirmar.

--- src/components/Totalizer/Totalizer.tsx
+++ src/components/Totalizer/Totalizer.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './totalizer.scss';
 import { FiTrash } from "react-icons/fi";
+import { FaCheck } from "react-icons/fa";
 import Modal from "../Modal/Modal";
 import { useRememberContext } from "../../contexts/remember";
 import { Product } from "../../types/types";
@@ -12,6 +13,7 @@ interface TotalizerProps {
 
 function Totalizer({ isVisible, total }: TotalizerProps) {
     const [isModalClearOpen, setIsModalClearOpen] = useState(false);
+    const [isModalConfirmOpen, setIsModalConfirmOpen] = useState(false);
     const { setProductsSelected, setResetProducts } = useRememberContext();
     const formattedTotal = Number(total()).toLocaleString('pt-BR', {
         style: 'currency',
@@ -31,15 +33,30 @@ function Totalizer({ isVisible, total }: TotalizerProps) {
         }
     };
 
+    const confirmOrder = () => {
+        try {
+            
+        } finally {
+            setIsModalConfirmOpen(false);
+        }
+    }
+
     return (
         <>  
             { isVisible() && (
                 <div className="totalizer-container">
+                    <div className="buttons-container" onClick={() => setIsModalClearOpen(true)}>
+                        <div className="button clear">
+                            <span><FiTrash /></span>
+                        </div>
+                    </div>
                     <div className="value-container">
                         Total: <span className="value">{ formattedTotal }</span>
                     </div>
-                    <div className="buttons">
-                        <span onClick={() => setIsModalClearOpen(true)}><FiTrash /></span>
+                    <div className="buttons-container" onClick={() => setIsModalConfirmOpen(true)}>
+                        <div className="button confirm">
+                            <span><FaCheck /></span>
+                        </div>
                     </div>
                 </div>
             )}
@@ -48,6 +65,12 @@ function Totalizer({ isVisible, total }: TotalizerProps) {
                 isOpen={isModalClearOpen} 
                 onClose={() => {setIsModalClearOpen(false)}} 
                 onConfirm={() => clearItems()}
+            />
+            <Modal 
+                title={"Deseja realmente confirmar o pedido?"}
+                isOpen={isModalConfirmOpen} 
+                onClose={() => {setIsModalConfirmOpen(false)}} 
+                onConfirm={() => confirmOrder()}
             />
         </>
     )
